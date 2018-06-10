dc656ea3a24a8ddf8a1ea6ee25247151b6f247d5
Gabriel Blankenburg <gabriel@factorbs.com.br>
2018-06-09 22:18:07 -0300

    Adicionado labels de cores

--- src/pages/todo/todo.ts
+++ src/pages/todo/todo.ts
@@ -9,17 +9,36 @@ import { FormControl } from '@angular/forms';
 })
 export class TodoPage {
 
-	private items:string[];
-	private fc;
+	private items:any[];
+	private task:FormControl;
+	private labels:any[];
+	private formConfig:any;
 
 	constructor(public navCtrl: NavController) {
 		this.items = [];
-		this.fc = new FormControl();
+		this.task = new FormControl();
+		this.labels = [
+			{color: '#C9FFE5', name:'Aero Blue'},
+			{color: '#6195ED', name:'Cornflower Blue'},
+			{color: '#7CB0A1', name:'Acapulco'},
+			{color: '#714693', name:'Affair'},
+			{color: '#93DFB8', name:'Algae Green'},
+			{color: '#E32636', name:'Alizarin Crimson'},
+			{color: '#9966CC', name:'Amethyst'},
+			{color: '#DFBE6F', name:'Apache'},
+			{color: '#DA6304', name:'Bamboo'},
+			{color: '#8C055E', name:'Cardinal Pink'},
+			{color: '#E3E3E3', name:'Mercury'},
+		];
+		this.formConfig = {
+			task: '',
+			label: {color: '#C9FFE5', name:'Aero Blue'},
+		}
 	}
 
 	private addButton() {
-		this.items.push(this.fc.value);
-		this.fc.setValue('');
+		this.items.push({task: this.formConfig.task, color: this.formConfig.label.color});
+		this.formConfig.task = '';
 	}
 
 	private remove(item) {
